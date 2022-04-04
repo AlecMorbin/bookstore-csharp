33f148ad684bf0fad3991479f96f8e3f37b96554
Alec <alemor98@hotmail.it>
2022-04-04 15:19:57 +0200

    Ultimato messaggio a schermo

--- BookStore CSharp/Program.cs
+++ BookStore CSharp/Program.cs
@@ -15,21 +15,22 @@ string title = "Clean Code: A Handbook of Agile Software Craftsmanship";
 string[] authors = {"Robert C. Martin" , "Dean Wampler"};
 long ISBN = 9780132350884;
 int nPages = 431;
+double weight = 0.660;
 double height = 23.5;
 double widht = 17.91;
 double depth = 2.54;
 double averageRating = 4.7;
-int nReviews = 0;
+int nReviews = 3308;
 bool isKindle = true;
 bool isCoverFlexible = true;
 
-Console.WriteLine("——— IL LIBRO DI OGGI: " +title+" di " +printAuthor(authors)+ "——————-" +
-    "\nISBN" + ISBN +
-    "\nNumero delle pagine: <<numeroPagine>> pagine" +
-    "\nPeso del libro: <<pesoLibro>> kg" +
-    "\nDimensioni del libro: <<larghezza>> cm x <<lunghezza>> cm x <<profondita>> cm" +
-    "\nInformazioni Amazon:" +
-    "\nNumero di recensioni: <<numeroRecensioni>> recensioni" +
-    "\nValutazione media: <<valutazioneMedia>> stelline" +
-    "\nKindle disponibile: Si o No" +
-    "\nCopertina flessibile disponibile: Si o No" );
+Console.WriteLine("——— IL LIBRO DI OGGI: " +title+" di " +printAuthor(authors)+ " ——————-" +
+    "\nISBN: " + ISBN +
+    "\nNumero delle pagine: " + nPages +"pagine" +
+    "\nPeso del libro: "+ weight + "kg" +
+    "\nDimensioni del libro: " + widht + "cm x"+ height + "cm x" + depth +" cm" +
+    "\nInformazioni Amazon: " +
+    "\nNumero di recensioni: " + nReviews+ " recensioni" +
+    "\nValutazione media: " + averageRating + " stelline" +
+    "\nKindle disponibile: " + ( isKindle ? "Si" : "No" ) +
+    "\nCopertina flessibile disponibile: " + (isCoverFlexible ? "Si" : "No") );
